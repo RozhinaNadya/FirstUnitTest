cc1370c191f4632198ef224ad60277da9fc8e8c1
RozhinaNadya <zhuk.09@mail.ru>
2022-08-03 11:53:50 +0200

    Initial Commit

---
+++ UnitTestDemo/UnitTestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestDemoApp.swift
+//  UnitTestDemo
+//
+//  Created by Надежда on 2022-08-03.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
